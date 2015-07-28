a4cb1574dccc8e133908a16f2ef067cb161414e9
Stefan Teixeira <stefanfk@gmail.com>
2015-07-28 01:49:31 -0300

    Commit inicial

---
+++ src/main/java/exemplo/pageobjects/CriarUsuarioPage.java
@@ -0,0 +1,93 @@
+package exemplo.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+
+public class CriarUsuarioPage extends BasePage
+{
+	@FindBy(id="msgSucesso")
+	private WebElement msgSucesso;
+	
+	@FindBy(id="nome")
+	private WebElement nome;
+	
+	public CriarUsuarioPage(WebDriver driver)
+	{
+		super(driver);
+	}
+
+	@Override
+	protected ExpectedCondition<?> getPageLoadCondition()
+	{
+		return ExpectedConditions.visibilityOf(nome);
+	}
+	
+	public WebElement getMsgSucesso()
+	{
+		return msgSucesso;
+	}
+	
+	public static class CriarUsuarioFormBuilder
+	{	
+		private WebDriver driver;
+		
+		public CriarUsuarioFormBuilder(WebDriver driver, String nome, String cpf)
+		{
+			this.driver = driver;
+			sendKeys("nome", nome);
+			sendKeys("cpf", cpf);
+		}
+		
+		public CriarUsuarioFormBuilder dataNascimento(String dataNascimento)
+		{
+			sendKeys("nasc", dataNascimento);
+			return this;
+		}
+		
+		public CriarUsuarioFormBuilder endereco(String endereco)
+		{
+			sendKeys("endereco", endereco);
+			return this;
+		}
+		
+		public CriarUsuarioFormBuilder bairro(String bairro)
+		{
+			sendKeys("bairro", bairro);
+			return this;
+		}
+		
+		public CriarUsuarioFormBuilder cidade(String cidade)
+		{
+			sendKeys("cidade", cidade);
+			return this;
+		}
+		
+		public CriarUsuarioFormBuilder estado(String estado)
+		{
+			Select estadoCombobox = new Select(this.driver.findElement(By.id("estado")));
+			estadoCombobox.selectByVisibleText(estado);
+			return this;
+		}
+		
+		public CriarUsuarioFormBuilder infosAdicionais(String infos)
+		{
+			sendKeys("infos", infos);
+			return this;
+		}
+		
+		public void submit()
+		{
+			this.driver.findElement(By.id("btn-submit")).click();
+		}
+		
+		private void sendKeys(String elementId, String text)
+		{
+			this.driver.findElement(By.id(elementId)).sendKeys(text);
+		}
+	}
+}
---
+++ src/test/java/tests/CriarUsuarioTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import exemplo.pageobjects.CriarUsuarioPage;
+
+public class CriarUsuarioTest extends BaseTest
+{
+	private CriarUsuarioPage usuarioPage;
+	
+	@Before
+	public void before()
+	{
+		usuarioPage = new CriarUsuarioPage(driver);
+	}
+	
+	@Test
+	public void criarUsuarioSomenteComCamposObrigatorios() throws InterruptedException
+	{
+		new CriarUsuarioPage.CriarUsuarioFormBuilder(driver, "Admin", "11145678906").submit();
+		
+		assertTrue(usuarioPage.getMsgSucesso().isDisplayed());
+	}
+	
+	@Test
+	public void criarUsuarioPreenchendoTodosOsCampos()
+	{
+		new CriarUsuarioPage.CriarUsuarioFormBuilder(driver, "Admin2", "12332199912")
+			.dataNascimento("01/07/1918")
+			.endereco("Av Presidente Vargas 1000")
+			.bairro("Centro")
+			.cidade("Rio de Janeiro")
+			.estado("Rio de Janeiro")
+			.infosAdicionais("Perto da Central do Brasil")
+			.submit();
+		
+		assertTrue(usuarioPage.getMsgSucesso().isDisplayed());
+	}
+}
